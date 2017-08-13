d1f17fbb95f976e58aad7de888c9415c6df72e46
ackbar03 <wdai03@gmail.com>
2017-08-13 18:46:45 +0800

    with sudo and space at end?

--- startup.sh
+++ startup.sh
@@ -3,10 +3,10 @@
 
 echo "deb https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu xenial/" | tee -a /etc/apt/sources.list 
 
-apt-get update
+sudo apt-get update
 
-apt-get -y install r-base
+sudo apt-get -y install r-base
 
-apt-get -y install r-base-dev
+sudo apt-get -y install r-base-dev
 
-nohup Rscript Run_v0-3_serverrun.R &
+sudo nohup Rscript Run_v0-3_serverrun.R &
