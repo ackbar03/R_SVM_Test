ae940d08d247d8dd89f2bab1cbbb142766e6a6b6
ackbar03 <wdai03@gmail.com>
2017-08-13 18:36:29 +0800

    No echo stuff?

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Startup script to download neccessary stuff
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu xenial/" | tee -a /etc/apt/sources.list
 
 apt-get update
 apt-get -y install r-base r-base-dev
