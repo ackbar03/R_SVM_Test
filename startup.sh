4678496e490832595085f6aa7e50798fe109767f
ackbar03 <wdai03@gmail.com>
2017-08-13 18:40:56 +0800

    With echo, rbase dev rbase seperate

--- startup.sh
+++ startup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Startup script to download neccessary stuff
 
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu xenial/" | tee -a /etc/apt/sources.list 
+
 apt-get update
 
-apt-get -y install r-base r-base-dev
+apt-get -y install r-base
+
+apt-get -y install r-base-dev
 
 nohup Rscript Run_v0-3_serverrun.R &
