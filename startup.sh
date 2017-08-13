e6771280eec025fcaac6d7e5931472e6070c9df3
ackbar03 <wdai03@gmail.com>
2017-08-13 18:32:49 +0800

    no sudo

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Startup script to download neccessary stuff
-sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu xenial/" | tee -a /etc/apt/sources.list
 
 apt-get update
 apt-get -y install r-base r-base-dev
